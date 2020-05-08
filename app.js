0696f6c79c1af4ce745cc2ec44d81206fe44f537
EduardoVirgilioDigitlhouse <evirgilio@digitalhouse.com>
2020-05-08 20:30:23 -0300

    Agrege mi familia
    Signed-off-by: EduardoVirgilioDigitlhouse <evirgilio@digitalhouse.com>

--- app.js
+++ app.js
@@ -2,3 +2,5 @@
 let miFamilia = ['Gloria','Indira','Victor','Luis'];
 console.log(miFamilia);
 console.log('Poco a poco mis amigas y amigos');
+let miFamilia = ['Jose','Alicia','Nicolas','Edu'];
+console.log(miFamilia);
