081d7aec885a996337e36f3c2e7ddffdb402dbad
Daniel Fuentes <=>
2020-05-08 21:50:33 -0300

    Realice un ciclo for of

--- app.js
+++ app.js
@@ -2,5 +2,8 @@
 let miFamilia = ['Gloria','Indira','Victor','Luis'];
 console.log(miFamilia);
 console.log('Poco a poco mis amigas y amigos');
-let miFamilia = ['Jose','Alicia','Nicolas','Edu'];
-console.log(miFamilia);
+let miFamiliaEdu = ['Jose','Alicia','Nicolas','Edu'];
+console.log(miFamiliaEdu);
+for (const familiar of miFamiliaEdu) {
+    console.log(familiar);
+}
